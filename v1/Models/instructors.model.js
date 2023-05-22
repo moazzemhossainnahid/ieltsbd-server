770e2cdf15501c38d78e257c56a8068a51ad4585
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-22 07:25:00 +0600

    add instructor model

--- v1/Models/instructors.model.js
+++ v1/Models/instructors.model.js
@@ -4,63 +4,79 @@ const validator = require("validator");
 
 const instructorsSchema = mongoose.Schema(
     {
+        name: {
+            type: String,
+            trim: true,
+            required: [true, "name is required"],
+        },
         title: {
             type: String,
             trim: true,
-            unique: true,
+            unique: false,
             required: [true, "title is required"],
-            minLength: [1, "Titile must be at least 3 characters."],
-            maxLength: [150, "Titile is too large"],
         },
-        category: {
+        email: {
             type: String,
+            unique: false,
             trim: true,
-            minLength: [1, "Category must be at least 3 characters."],
-            maxLength: [20, "Category is too large"],
-        },
-        subCategory: {
-            type: Array,
-            trim: true,
-            minLength: [1, "subCategory must be at least 3 characters."],
-            maxLength: [100, "subCategory is too large"],
         },
         description: {
             type: String,
-            required: [true, "Description is required"],
             trim: true,
         },
-        authorName: {
+        phone: {
             type: String,
             trim: true,
-            required: [true, "authorName is required"],
-            minLength: [1, "authorName must be at least 3 characters."],
-            maxLength: [100, "authorName is too large"],
         },
-        authorEmail: {
+        gender: {
             type: String,
             trim: true,
-            required: [true, "authorMail is required"],
-            minLength: [3, "authorMail must be at least 3 characters."],
-            maxLength: [100, "authorMail is too large"],
+        },
+        img: {
+            type: String,
+            trim: true,
+        },
+        professionLevel: {
+            type: String,
+            trim: true,
+        },
+        teacherCategory: {
+            type: String,
+            trim: true,
+        },
+        totalTechingExp: {
+            type: Number,
+            trim: true,
+        },
+        techingType: {
+            type: String,
+            trim: true,
+        },
+        specialist: {
+            type: String,
+            trim: true,
+        },
+        address: {
+            type: String,
+            trim: true,
+        },
+        education: {
+            type: Array,
+            trim: true,
+        },
+        feeDetails: {
+            type: Number,
+            trim: true,
+        },
+        social_link: {
+            type: Object,
+            trim: true,
         },
         status: {
             type: String,
-            default: "unapprove",
-            enum: ["approve", "unapprove"],
-        },
-        authorAvatar: {
-            type: String,
-            trim: true,
-        },
-        cover: {
-            type: String,
-            trim: true,
-        },
-        createdAt: {
-            type: String,
-            trim: true,
-        },
-
+            default: "active",
+            enum: ["active", "deactive"],
+        }
     },
     {
         timestamps: true,
